82fd8e72a2804b9e20cd84b6b90e6ac3943b92dc
Manatazach Lawya <manatazach144@Manatazachs-MacBook-Pro.local>
2015-02-13 20:44:03 -0500

    fix Ajax to submit items and delete items without refreshing

--- app/assets/javascripts/todo_items.js
+++ app/assets/javascripts/todo_items.js
@@ -1,20 +1,9 @@
 //I need to select any li with data attach to it, and send an ajax request to delete it
 $(document).ready(function(){
-
-		$('.destroy').click(function(){
+		$('ul').delegate('.destroy', 'click', function(){
 			var myUrl = '/todo_items/' + $(this).data('todo-items');
 			//console.log($(self).data('todo-items'));
 			var li = $(this).parents('li');
 			$.post(myUrl, {"_method":"DELETE"}, function(){ $(li).remove()}, 'script')
 		});
-
 });
-
-// $(document).ready(function() {
-// 	$('#item').click(function(e) {
-// 	  e.preventDefault();
-// 	  $.ajax({
-// 	    type: 'DELETE',
-// 	    url: '/todo_items/'+});
-// 	});
-// });
